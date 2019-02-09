wrongway213/android_device_oneplus_msm8998-common-1

msm8998: Import power HAL for wahoo for EAS
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

prebuilt/vendor/bin/init.qcom.power.sh
@@ -1,0 +1,31 @@
+#! /vendor/bin/sh
+
+# Enable bus-dcvs
+for cpubw in /sys/class/devfreq/*qcom,cpubw*
+do
+    echo "bw_hwmon" > $cpubw/governor
+    echo 50 > $cpubw/polling_interval
+    echo 1525 > $cpubw/min_freq
+    echo "3143 5859 11863 13763" > $cpubw/bw_hwmon/mbps_zones
+    echo 4 > $cpubw/bw_hwmon/sample_ms
+    echo 34 > $cpubw/bw_hwmon/io_percent
+    echo 20 > $cpubw/bw_hwmon/hist_memory
+    echo 10 > $cpubw/bw_hwmon/hyst_length
+    echo 0 > $cpubw/bw_hwmon/low_power_ceil_mbps
+    echo 34 > $cpubw/bw_hwmon/low_power_io_percent
+    echo 20 > $cpubw/bw_hwmon/low_power_delay
+    echo 0 > $cpubw/bw_hwmon/guard_band_mbps
+    echo 250 > $cpubw/bw_hwmon/up_scale
+    echo 1600 > $cpubw/bw_hwmon/idle_mbps
+done
+
+for memlat in /sys/class/devfreq/*qcom,memlat-cpu*
+do
+    echo "mem_latency" > $memlat/governor
+    echo 10 > $memlat/polling_interval
+    echo 400 > $memlat/mem_latency/ratio_ceil
+done
+
+echo "cpufreq" > /sys/class/devfreq/soc:qcom,mincpubw/governor
+
+setprop sys.post_boot.parsed 1

LeChenOS/device_oneplus_msm8998-common-2

prebuilt/vendor/bin/init.qcom.power.sh
@@ -1,0 +1,31 @@
+#! /vendor/bin/sh
+
+# Enable bus-dcvs
+for cpubw in /sys/class/devfreq/*qcom,cpubw*
+do
+    echo "bw_hwmon" > $cpubw/governor
+    echo 50 > $cpubw/polling_interval
+    echo 1525 > $cpubw/min_freq
+    echo "3143 5859 11863 13763" > $cpubw/bw_hwmon/mbps_zones
+    echo 4 > $cpubw/bw_hwmon/sample_ms
+    echo 34 > $cpubw/bw_hwmon/io_percent
+    echo 20 > $cpubw/bw_hwmon/hist_memory
+    echo 10 > $cpubw/bw_hwmon/hyst_length
+    echo 0 > $cpubw/bw_hwmon/low_power_ceil_mbps
+    echo 34 > $cpubw/bw_hwmon/low_power_io_percent
+    echo 20 > $cpubw/bw_hwmon/low_power_delay
+    echo 0 > $cpubw/bw_hwmon/guard_band_mbps
+    echo 250 > $cpubw/bw_hwmon/up_scale
+    echo 1600 > $cpubw/bw_hwmon/idle_mbps
+done
+
+for memlat in /sys/class/devfreq/*qcom,memlat-cpu*
+do
+    echo "mem_latency" > $memlat/governor
+    echo 10 > $memlat/polling_interval
+    echo 400 > $memlat/mem_latency/ratio_ceil
+done
+
+echo "cpufreq" > /sys/class/devfreq/soc:qcom,mincpubw/governor
+
+setprop vendor.post_boot.parsed 1